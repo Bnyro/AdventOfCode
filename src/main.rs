a29daa0e2885a3ba5835649d2a59ec2271b2d8c2
Bnyro <bnyro@tutanota.com>
2022-12-07 12:33:21 +0100

    day 3 part 2

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,5 @@ fn main() {
     println!("2) 2: {}", solutions::day_2::part_two());
 
     println!("3) 1: {}", solutions::day_3::part_one());
-    // println!("3) 2: {}", solutions::day_3::part_two());
+    println!("3) 2: {}", solutions::day_3::part_two());
 }
--- src/solutions/day_3.rs
+++ src/solutions/day_3.rs
@@ -24,6 +24,32 @@ pub fn part_one() -> String {
         .to_string()
 }
 
+pub fn part_two() -> String {
+    let content = read_file(3);
+    let lines: Vec<&str> = content.lines().collect();
+    let mut groups: Vec<Vec<&str>> = vec![];
+    let mut group: Vec<&str> = vec![];
+    for i in 0..lines.len() {
+        group.push(lines[i]);
+        if group.len() == 3 {
+            groups.push(group);
+            group = vec![];
+        }
+    }
+    groups
+        .iter()
+        .map(|group| {
+            let common_char = group[0]
+                .chars()
+                .filter(|c| group[1].contains(*c))
+                .filter(|c| group[2].contains(*c))
+                .collect::<Vec<char>>();
+            get_char_value(*common_char.first().unwrap())
+        })
+        .sum::<u32>()
+        .to_string()
+}
+
 fn get_char_value(character: char) -> u32 {
     let order = character as u32;
     if character.is_uppercase() {
