b46df0dd6f3f3383f60cd215743632af73beb551
Bnyro <bnyro@tutanota.com>
2022-12-07 11:30:20 +0100

    finish day two

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,7 @@ pub mod util;
 fn main() {
     println!("1) 1: {}", solutions::day_1::part_one());
     println!("1) 2: {}", solutions::day_1::part_two());
+
     println!("2) 1: {}", solutions::day_2::part_one());
+    println!("2) 2: {}", solutions::day_2::part_two());
 }
--- src/solutions/day_2.rs
+++ src/solutions/day_2.rs
@@ -6,11 +6,6 @@ pub fn part_one() -> String {
     for battle in file.lines() {
         let opponent_shape = battle.chars().nth(0).unwrap().to_string();
         let own_shape = battle.chars().nth(2).unwrap().to_string();
-        let value = match own_shape.as_str() {
-            "X" => 1,
-            "Y" => 2,
-            _ => 3,
-        };
         let mut win = 0;
         if (own_shape == "X" && opponent_shape == "A")
             || (own_shape == "Y" && opponent_shape == "B")
@@ -23,7 +18,55 @@ pub fn part_one() -> String {
         {
             win = 6
         }
-        score = score + value + win
+        score = score + get_value(own_shape.as_str()) + win
     }
     score.to_string()
+}
+
+pub fn part_two() -> String {
+    let file = read_file(2);
+    file.lines()
+        .map(|line| {
+            let opponent_choice = line.chars().nth(0).unwrap();
+            let elf_hint = line.chars().nth(2).unwrap();
+            get_score(opponent_choice, elf_hint)
+        })
+        .sum::<i32>()
+        .to_string()
+}
+
+fn get_value(choice: &str) -> i32 {
+    match choice {
+        "X" | "A" => 1,
+        "Y" | "B" => 2,
+        _ => 3,
+    }
+}
+
+fn get_score(opponent_choice: char, elf_hint: char) -> i32 {
+    let opp_choice = opponent_choice.to_string();
+    let own_choice;
+    let score: i32 = match elf_hint.to_string().as_str() {
+        "Y" => {
+            own_choice = opp_choice.as_str();
+            3
+        }
+        "X" => {
+            own_choice = match opp_choice.as_str() {
+                "A" => "C",
+                "B" => "A",
+                _ => "B",
+            };
+            0
+        }
+        _ => {
+            own_choice = match opp_choice.as_str() {
+                "A" => "B",
+                "B" => "C",
+                _ => "A",
+            };
+            6
+        }
+    };
+    score + get_value(own_choice)
 }
