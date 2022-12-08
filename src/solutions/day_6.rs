ba14b7a02c864832ac9c2294c26964332be65abf
Bnyro <bnyro@tutanota.com>
2022-12-08 15:00:14 +0100

    improve day 6

--- src/solutions/day_6.rs
+++ src/solutions/day_6.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 use crate::util::read_file;
 
 pub fn part_one() -> String {
@@ -10,21 +12,14 @@ pub fn part_two() -> String {
 
 fn solve(distinct_chars: usize) -> String {
     let chars: Vec<char> = read_file(6).chars().collect();
-    let mut index: u32 = 0;
-    let mut current_index = 0;
-    while index == 0 {
-        let mut four_chars: Vec<char> = vec![];
-        for i in 0..distinct_chars {
-            four_chars.push(chars[current_index + i]);
-        }
-        let mut cleaned_chars = four_chars.clone();
-        cleaned_chars.sort();
-        cleaned_chars.dedup();
-        if cleaned_chars.len() == four_chars.len() {
-            index = (current_index + distinct_chars) as u32;
-            break;
-        }
-        current_index = current_index + 1;
-    }
-    index.to_string()
+    let seq = chars
+        .windows(distinct_chars)
+        .enumerate()
+        .find(|&(_index, c)| {
+            let filtered = c.iter().collect::<BTreeSet<&char>>();
+            filtered.len() == c.len()
+        })
+        .unwrap();
+
+    (seq.0 + distinct_chars).to_string()
 }
